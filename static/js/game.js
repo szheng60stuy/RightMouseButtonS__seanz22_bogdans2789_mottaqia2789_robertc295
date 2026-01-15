91f4957f02ee046449ad63add6467db017152e98
BogdanSotnikov <bogdans2789@nycstudents.net>
2026-01-14 23:39:27 -0500

    Progress from class

--- static/js/game.js
+++ static/js/game.js
@@ -1,14 +1,16 @@
 let players = [];
 let pIndex = 0;
+let terr = ["America", "China", "England"];
+let out = NULL;
 
 async function Setup()
 {
   players = document.getElementById('plyrs').children;
+  out = document.getElementById('outlog');
 }
 
-function Atck(target)
+function Atck(target, source)
 {
-
+  out.innerText = source + "is attacking" + target;
+  
 }
-
-function 
